9fc5f654e46d67e7689fab23649438fbcccf7d83
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-09-10 20:27:10 +0800

    It Worksgit add -Agit add -A

--- do_rsync_clear.sh
+++ do_rsync_clear.sh
@@ -2,28 +2,28 @@ MISSING_STATUS=`cat /home/jline/missing_status.txt`
 
 if [ $MISSING_STATUS = ALL_FILES ]; then
 
-    at 18:33 < do_rsync.sh
-    echo "echo \"[18:37AM]: All data has been tranferred to Ozstar.\" >> /home/jline/t2.txt" | at 18:36
-    at 18:37 < clear_data.sh
-    echo "echo \"[18:37AM]: Data directory has been cleared.\" >> /home/jline/t2.txt" | at 18:38
-    echo "echo \"\" >> /home/jline/t2.txt" | at 18:39
-    echo "echo \"\" >> /home/jline/t2.txt" | at 18:39
+    at 06:05 < do_rsync.sh
+    echo "echo \"[06:20AM]: All data has been tranferred to Ozstar.\" >> /home/jline/t2.txt" | at 06:19
+    at 06:20 < clear_data.sh
+    echo "echo \"[06:20AM]: Data directory has been cleared.\" >> /home/jline/t2.txt" | at 06:21
+    echo "echo \"\" >> /home/jline/t2.txt" | at 06:22
+    echo "echo \"\" >> /home/jline/t2.txt" | at 06:22
 
 
 elif [ $MISSING_STATUS = MISSING_FILES ]; then
     
-    at 18:33 < do_rsync.sh
-    echo "echo \"[18:37AM]: All data has been tranferred to Ozstar. \" >> /home/jline/t2.txt" | at 18:36
-    echo "echo \"[18:37AM]: Did not clear data. Something went wrong yesterday.\" >> /home/jline/t2.txt" | at 18:38
-    echo "echo \"\" >> /home/jline/t2.txt" | at 18:39
-    echo "echo \"\" >> /home/jline/t2.txt" | at 18:39
+    at 06:05 < do_rsync.sh
+    echo "echo \"[06:20AM]: All data has been tranferred to Ozstar. \" >> /home/jline/t2.txt" | at 06:19
+    echo "echo \"[06:20AM]: Did not clear data. Something went wrong yesterday.\" >> /home/jline/t2.txt" | at 06:21
+    echo "echo \"\" >> /home/jline/t2.txt" | at 06:22
+    echo "echo \"\" >> /home/jline/t2.txt" | at 06:22
 
 
 else
 
-    echo "echo \"[18:33AM]: No rsync or clear_data required as there were no data.\" >> /home/jline/t2.txt" | at  18:38
-    echo "echo \"\" >> /home/jline/t2.txt" | at 18:39
-    echo "echo \"\" >> /home/jline/t2.txt" | at 18:39
+    echo "echo \"[06:05AM]: No rsync or clear_data required as there were no data.\" >> /home/jline/t2.txt" | at 06:21
+    echo "echo \"\" >> /home/jline/t2.txt" | at 06:22
+    echo "echo \"\" >> /home/jline/t2.txt" | at 06:22
 
 fi
 
--- make_night_schedule.sh
+++ make_night_schedule.sh
@@ -1,13 +1,13 @@
 # This script creates a queue of at jobs for a 24 hour period
 # Needs to be executed before 6:00AM everyday
 
-#OBS_LENGHT=1800
-#TODAY=`date +'%Y-%m-%d-%H:%M' -d '6am'`
-#TOMORROW=`date +'%Y-%m-%d-%H:%M' -d '6am +1 day'`
+OBS_LENGHT=1800
+START=`date +'%Y-%m-%d-%H:%M' -d '6am'`
+STOP=`date +'%Y-%m-%d-%H:%M' -d '6am +1 day'`
 
-START=2019-09-10-17:40
-STOP=2019-09-10-18:28
-OBS_LENGHT=60
+# START=2019-09-10-17:40
+# STOP=2019-09-10-18:28
+# OBS_LENGHT=60
 
 
 python make_night_schedule.py --time_obs=$OBS_LENGHT --start_date=$START \
